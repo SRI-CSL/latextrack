2ceab997db04546f7559f20a05d72f2d901699ac
Linda Briesemeister <linda.briesemeister@sri.com>
2012-01-24 08:41:48 -0600

    Emacs Lisp files now back into distributable and updated README

---
+++ ltc-emacs/src/main/java/com/sri/ltc/emacs/Dummy.java
@@ -0,0 +1,16 @@
+/**
+ ************************ 80 columns *******************************************
+ * Dummy
+ *
+ * Created on 1/12/12.
+ *
+ * Copyright 2009-2010, SRI International.
+ */
+package com.sri.ltc.emacs;
+
+/**
+ * Dummy class to have maven not barf.
+ * @author linda
+ */
+public class Dummy {
+}
